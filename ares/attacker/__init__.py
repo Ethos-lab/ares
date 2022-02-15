ac2fa876316c0618d4a75f7c7ea110c83d886285
Ajay Sarjoo <ajay.sarjoo@outlook.com>
2022-02-14 22:36:50 -0500

    more additions to AttackerAgent class

--- ares/attacker/__init__.py
+++ ares/attacker/__init__.py
@@ -6,9 +6,9 @@ from attacks import *
 
 
 class AttackerAgent:
-    def __init__(self, model: PyTorchClassifier, attack_type: Attack, device: torch.device):
+    def __init__(self, model: nn.Module, attack_type: str, device: torch.device, **model_kwargs):
         self.num_steps = 0
-        self.model = model
+        self.model = PyTorchClassifier(model, model_kwargs)
         self.attack_type = attack_type
         self.device = device
 
@@ -21,6 +21,20 @@ class AttackerAgent:
         self.num_steps += 1
         return image_adv
 
-    def attack(self, attack: Attack, **kwargs):
-        wrapped_art_model = self.attack_type(self.model)
-        return wrapped_art_model
+    def attack(self, image: torch.Tensor, label: torch.Tensor) -> nn.Module:
+        attack = self.get_attack_type()
+        with torch.enable_grad():
+            bx = attack.generate(x=image.cpu().numpy(), y=label.cpu().numpy())
+        image_adv = torch.tensor(bx, device=self.device)
+        self.num_steps += 1
+        return image_adv
+
+    def get_attack_type(self):
+        if self.attack_type == "pgd":
+            return PGDAttack(self.model)
+        elif self.attack_type == "autopgd":
+            return AutoPGDAttack(self.model)
+        elif self.attack_type == "autoattack":
+            return AutoAttack(self.model)
+        else:
+            raise Exception(f"Attack type {self.attack_type} not supported")
